92e0211d19187a066aeaebc52cb71093b00fa637
Albert Felix <albertfelixleo@gmail.com>
2021-01-13 11:05:07 +0530

    dynamic url

--- templates/app.py
+++ templates/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -9,6 +9,22 @@ def home():
 @app.route('/profile/<name>')
 def profile(name):
     return(f"This is profile page of {name}")
+
+@app.route('/admin')
+def hello_admin():
+    return("Hello Admin")
+
+@app.route('/guest/<name>')
+def hello_guest(name):
+    return(f"Hello guest - {name}")
+
+@app.route('/user/<user>')
+def hello_user(user):
+    if user == 'admin':
+        return(redirect(url_for('hello_admin')))
+    else:
+        return(redirect(url_for('hello_guest', name=user)))
+
 
 if __name__ == '__main__':
     app.run()

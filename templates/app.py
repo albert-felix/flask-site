967ea66ef7c1b8edd3d59afc59778171db3cc9bc
Albert Felix <albertfelixleo@gmail.com>
2021-01-13 10:35:58 +0530

    used variables from url

--- templates/app.py
+++ templates/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return('Homepage')
 
+@app.route('/profile/<name>')
+def profile(name):
+    return(f"This is profile page of {name}")
+
 if __name__ == '__main__':
     app.run()
